daeefb86e5ffea76ac58a2e8eac15850c6bef6ea
musabinabdullah5678 <musabinabdullah5678@gmail.com>
2025-07-25 16:16:09 +0500

    Add files via upload

---
+++ PF/Tables via Loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int a;
+	cout<<"enter the number for the table : ";
+	cin>>a;
+	for(int i=0;i<=12;i++){
+	
+		cout<<a<<"*"<<i<<"=";
+		cout<<a*i<<endl;
+	}
+}
